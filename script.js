088f13caea1720fe1a6714e0e6d7000c84687a2f
Rushali <rushalipatnaik18@gmail.com>
2022-01-11 22:05:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject = mediaStream;
+       videoElement.onloadedmetadata = () => {
+           videoElement.play();
+       }
+    } catch (error) {
+        // Catch error here
+        console.log('whoops, error here:', error)
+    }
+}
+
+//On load
+selectMediaStream();
